ea305b96a39b1e5b358ee1b78accf80af6f9a0a1
dungnt0605 <sdvndung051230@gmail.com>
2024-05-15 13:15:54 +0700

    fix:bannerBlog

--- FE/tailwind.config.js
+++ FE/tailwind.config.js
@@ -2,10 +2,13 @@ const flowbite = require("flowbite-react/tailwind");
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	important: true,
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
-	theme: {
-		extend: {},
-	},
-	plugins: [flowbite.plugin()],
+  important: true,
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
+  theme: {
+    extend: {},
+    backgroundImage: {
+      "banner-blog": `url('./src/assets/img/headerBlog.webp')`,
+    },
+  },
+  plugins: [flowbite.plugin()],
 };
