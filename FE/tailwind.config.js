653c1d788c6db6b247edf56fc873f614e3c369ef
dungnt0605 <sdvndung051230@gmail.com>
2024-05-15 23:33:49 +0700

    fix:bannerHome

--- FE/tailwind.config.js
+++ FE/tailwind.config.js
@@ -8,6 +8,11 @@ export default {
     extend: {},
     backgroundImage: {
       "banner-blog": `url('./src/assets/img/headerBlog.webp')`,
+      "banner-home1": `url('./src/assets/img/HeaderBanner (1).png')`,
+      "banner-home2": `url('./src/assets/img/HeaderBanner (2).png')`,
+      "banner-home3": `url('./src/assets/img/HeaderBanner (3).png')`,
+      "banner-home4": `url('./src/assets/img/HeaderBanner (4).png')`,
+      "banner-home6": `url('./src/assets/img/HeaderBanner (6).png')`,
     },
   },
   plugins: [flowbite.plugin()],
