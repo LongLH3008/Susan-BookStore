8d13d31245641b046c2f4e10f63a0202c7eddcbe
phng2580 <phuongbui2580@gmail.com>
2024-08-03 20:09:10 +0700

    update contact

--- FE/src/client/pages/Contact.tsx
+++ FE/src/client/pages/Contact.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 import FormContact from "../components/page/contact/FormContact";
 import Info from "../components/page/contact/info";
+import Map from "../components/page/contact/map";
 
 type Props = {};
 
 const Contact = (props: Props) => {
   return (
     <div className="min-[320px]:px-[5%] xl:px-[11.5%] 2xl:px-[17.5%] mt-5">
-      <div className="grid grid-cols-12 gap-4">
-        {" "}
-        <FormContact />
-        <Info />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div className="col-span-1">
+          <FormContact />
+        </div>
+        <div className="col-span-1">
+          <Info />
+        </div>
+      </div>
+      <div>
+        <Map />
       </div>
     </div>
   );
