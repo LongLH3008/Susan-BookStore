e70774f5ffc44f84cf4b4c3598616f725697db03
phng2580 <phuongbui2580@gmail.com>
2024-09-27 16:12:18 +0700

    done

--- FE/src/pages/(dashboard)/Products/FormAttr.tsx
+++ FE/src/pages/(dashboard)/Products/FormAttr.tsx
@@ -5,11 +5,6 @@ import ProductForm from "./productForm";
 const FormAttr = () => {
   return (
     <div>
-      <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
-        <p className="text-2xl font-bold text-gray-800 dark:text-gray-50">
-          Products add
-        </p>
-      </div>
       <ProductForm />
     </div>
   );
