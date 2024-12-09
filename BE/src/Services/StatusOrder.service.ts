bdb6596fa0a8a0aa8f3d6c84c75ed41105c096a0
kangzzz <bkak60@gmail.com>
2024-12-09 20:27:45 +0700

    X

--- BE/src/Services/StatusOrder.service.ts
+++ BE/src/Services/StatusOrder.service.ts
@@ -1,101 +1,101 @@
-import { stat } from "fs";
+import {stat} from "fs";
 import Order from "../models/Order.model";
 import Book from "../models/Book.model";
 import SendEmalCheckOutOrder from "../helper/EmailOrder";
 import emailQueue from "../queues/mail.queue";
-import { InternalServerError, ResourceNotFoundError } from "../cores/error.response";
-import { Interface } from "node:readline/promises";
+import {InternalServerError, ResourceNotFoundError} from "../cores/error.response";
+import {Interface} from "node:readline/promises";
 
 class StatusOrerService {
 
 
-  // pending = "pending", confirmed = "confirmed", shipped = "shipped", cancelled = "cancelled", "success" = "success"
-  static async UpdateStatusOrderForAdmin(id: string, state: string) {
-    try {
-      // checkId Order
-      const validStatuses = [
-        "pending",
-        "confirmed",
-        "shipped",
-        "cancelled",
-        "success",
-      ];
-      if (!validStatuses.includes(state)) {
-        throw new Error(
-          "Invalid status. Valid statuses: pending, confirmed, shipped, cancelled"
-        );
-      }
-      // udpate
-      const udpateOrder = await Order.findByIdAndUpdate(
-        id,
-        { state: state },
-        { new: true }
-      );
+    // pending = "pending", confirmed = "confirmed", shipped = "shipped", cancelled = "cancelled", "success" = "success"
+    static async UpdateStatusOrderForAdmin(id: string, state: string) {
+        try {
+            // checkId Order
+            const validStatuses = [
+                "pending",
+                "confirmed",
+                "shipped",
+                "cancelled",
+                "success",
+            ];
+            if (!validStatuses.includes(state)) {
+                throw new Error(
+                    "Invalid status. Valid statuses: pending, confirmed, shipped, cancelled"
+                );
+            }
+            // udpate
+            const udpateOrder = await Order.findByIdAndUpdate(
+                id,
+                {state: state},
+                {new: true}
+            );
 
-      if (!udpateOrder) {
-        throw new Error("Order not found");
-      }
-
-
-      const shouldSubtractStockStatus = ["confirmed", "shipped", "success"]
+            if (!udpateOrder) {
+                throw new Error("Order not found");
+            }
 
 
-      if (shouldSubtractStockStatus.includes(state)) {
-      
+            const shouldSubtractStockStatus = ["confirmed", "shipped", "success"]
 
 
-        if (!udpateOrder.isSubtractedStock) {
-          const productsFromServer = await Promise.all(udpateOrder.products.map(item => Book.findOne({ _id: item.bookId })))
+            if (shouldSubtractStockStatus.includes(state)) {
 
-          if (productsFromServer.some(book => !book)) throw new ResourceNotFoundError("Book not found!")
-  
-          if (productsFromServer.some((item, index) => item!.stock < udpateOrder.products[index].quantity)) throw new InternalServerError("Some books is out of stock, please check again!")
-          const bulkUpdateOperations = udpateOrder.products.map((item: any) => ({
-            updateOne: {
-              filter: { _id: item.bookId },
-              update: { $inc: { stock: -item.quantity } },
-            },
-          }));
 
-          await Book.bulkWrite(bulkUpdateOperations);
-          return await Order.findByIdAndUpdate(id, { isSubtractedStock: true }, { new: true })
+                if (!udpateOrder.isSubtractedStock) {
+                    const productsFromServer = await Promise.all(udpateOrder.products.map(item => Book.findOne({_id: item.bookId})))
+
+                    if (productsFromServer.some(book => !book)) throw new ResourceNotFoundError("Book not found!")
+
+                    if (productsFromServer.some((item, index) => item!.stock < udpateOrder.products[index].quantity)) throw new InternalServerError("Some books is out of stock, please check again!")
+                    const bulkUpdateOperations = udpateOrder.products.map((item: any) => ({
+                        updateOne: {
+                            filter: {_id: item.bookId},
+                            update: {$inc: {stock: -item.quantity, sold: item.quantity}},
+                        },
+                    }));
+
+                    await Book.bulkWrite(bulkUpdateOperations);
+                    return await Order.findByIdAndUpdate(id, {isSubtractedStock: true}, {new: true})
+                }
+
+            }
+            if (state === "cancelled") {
+                if (udpateOrder.isSubtractedStock) {
+                    const bulkUpdateOperations = udpateOrder.products.map((item: any) => ({
+                        updateOne: {
+                            filter: {_id: item.bookId},
+                            update: {$inc: {stock: item.quantity, sold: -item.quantity}},
+                        },
+                    }));
+
+                    await Book.bulkWrite(bulkUpdateOperations);
+                    (udpateOrder.userInfo.email, udpateOrder.trackingNumber)
+
+                }
+                emailQueue.push({
+                    type: "cancelOrder",
+                    email: udpateOrder.userInfo.email,
+                    trackingNumber: udpateOrder.trackingNumber
+
+                })
+                return await Order.findByIdAndUpdate(id, {isSubtractedStock: false}, {new: true})
+            }
+            return udpateOrder;
+        } catch (err: any) {
+            console.log(err.message);
+            throw new Error(err.message);
         }
+    }
 
-      }
-      if (state === "cancelled") {
-        if (udpateOrder.isSubtractedStock) {
-          const bulkUpdateOperations = udpateOrder.products.map((item: any) => ({
-            updateOne: {
-              filter: { _id: item.bookId },
-              update: { $inc: { stock: item.quantity } },
-            },
-          }));
-
-          await Book.bulkWrite(bulkUpdateOperations);
-          (udpateOrder.userInfo.email, udpateOrder.trackingNumber)
-
+    static async UpdateStatusOrderForClient(id: number) {
+        try {
+        } catch (err: any) {
+            console.log(err.message);
+            throw new Error(err.message);
         }
-        emailQueue.push({
-          type: "cancelOrder",
-          email: udpateOrder.userInfo.email,
-          trackingNumber: udpateOrder.trackingNumber
-
-        })
-        return await Order.findByIdAndUpdate(id, { isSubtractedStock: false }, { new: true })
-      }
-      return udpateOrder;
-    } catch (err: any) {
-      console.log(err.message);
-      throw new Error(err.message);
     }
-  }
-  static async UpdateStatusOrderForClient(id: number) {
-    try {
-    } catch (err: any) {
-      console.log(err.message);
-      throw new Error(err.message);
-    }
-  }
 }
 
 export default StatusOrerService;
